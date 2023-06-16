c395143323523a1cdbc23d3d212503d16300ea44
Agustin Aleksandroff <agustinaleks@gmail.com>
2023-06-16 16:57:31 -0300

    se agrega ejercicio 2 + Readme + html texto

--- Carrito/tarea2.js
+++ Carrito/tarea2.js
@@ -79,6 +79,39 @@ class Carrito {
             console.error(`Error al intentarinsertararticulo no existente:${error}`); 
         }
     }
+
+
+    eliminarProducto(sku, cantidad) {
+        return new Promise(async (resolve, reject) => {
+          try {
+            console.log(`Eliminando ${cantidad} ${sku}`);
+            const producto = await findProductBySku(sku);
+            
+      
+            // Buscar el producto en el carrito
+            const productoExistente = this.productos.find(producto => producto.sku === sku);
+      
+            if (!productoExistente) {
+              reject(`El producto ${sku} no existe en el carrito.`);
+              return;
+            }
+      
+            if (cantidad < productoExistente.cantidad) {
+              productoExistente.cantidad -= cantidad;
+              console.log(`Se han eliminado ${cantidad} unidades de ${sku} del carrito.`);
+            } else {
+              // Elimina el producto del carrito
+              const index = this.productos.indexOf(productoExistente);
+              this.productos.splice(index, 1);
+              console.log(`Se ha eliminado el producto  ${sku} completamente del carrito.`);
+            }
+      
+            resolve();
+          } catch (error) {
+            reject(`Error al eliminar el producto del carrito: ${error}`);
+          }
+        });
+      }
     
 }
 class ProductoEnCarrito {
@@ -112,35 +145,16 @@ function findProductBySku(sku){
 
 
 const carrito = new Carrito();
-carrito.agregarProducto('WE328NJ', 2);
-carrito.agregarProducto('WE328NJ', 3);
-carrito.agregarProducto('OL883YE', 3);
-carrito.agregarProducto('OL883Y7', 3);
+carrito.agregarProducto('WE328NJ', 7);
+// carrito.agregarProducto('WE328NJ', 3);
+// carrito.agregarProducto('OL883YE', 3);
+// carrito.agregarProducto('OL883Y7', 3);
+carrito.eliminarProducto('WE328NJ', 2)   //'WE328NJ', 7  
+  .then(() => {
+    console.log('Producto eliminado del carrito correctamente.');
+  })
+  .catch((error) => {
+    console.error('Error al eliminar el producto del carrito:', error);
+  });
 
 
-
-/*
-En el archivo tarea2.js podemos encontrar un código de un supermercado que vende productos.
-El código contiene 
-    - una clase Producto que representa un producto que vende el super
-    - una clase Carrito que representa el carrito de compras de un cliente
-    - una clase ProductoEnCarrito que representa un producto que se agrego al carrito
-    - una función findProductBySku que simula una base de datos y busca un producto por su sku
-El código tiene errores y varias cosas para mejorar / agregar
-​
-Ejercicios
-1) Arreglar errores existentes en el código
-    a) Al ejecutar agregarProducto 2 veces con los mismos valores debería agregar 1 solo producto con la suma de las cantidades.    
-    b) Al ejecutar agregarProducto debería actualizar la lista de categorías solamente si la categoría no estaba en la lista.
-    c) Si intento agregar un producto que no existe debería mostrar un mensaje de error.
-​
-2) Agregar la función eliminarProducto a la clase Carrito
-    a) La función eliminarProducto recibe un sku y una cantidad (debe devolver una promesa)
-    b) Si la cantidad es menor a la cantidad de ese producto en el carrito, se debe restar esa cantidad al producto
-    c) Si la cantidad es mayor o igual a la cantidad de ese producto en el carrito, se debe eliminar el producto del carrito
-    d) Si el producto no existe en el carrito, se debe mostrar un mensaje de error
-    e) La función debe retornar una promesa
-​
-3) Utilizar la función eliminarProducto utilizando .then() y .catch()
-​
-*/
